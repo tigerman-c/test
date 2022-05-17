81a05bf5986987df1d6c928feaad3182a1cf1678
陈凯文 <chenkaiwen@netseekgaming.com>
2022-05-17 10:32:43 +0800

    add: 柯里化demo

---
+++ js/curry.js
@@ -0,0 +1,10 @@
+const curry = (fn, arr = []) => (...args) => (a => a.length === fn.length ? fn(...a) : curry(fn, a))([...arr, ...args])
+let curryPlus = curry((a, b, c, d) => console.log(a + b + c + d))
+
+curryPlus(1, 2, 3, 4) //返回10
+curryPlus(1, 2)(4)(3) //返回10
+curryPlus(1, 2)(3, 4) //返回10
+curryPlus(1)(2)(3)(4) //返回10
+
+
+

51304c75b5b29c6b6783ffdc29c79ae474610b25
我是好人 <40545044+kwSir-chen@users.noreply.github.com>
2023-03-05 18:58:22 +0800

    feat eventbus添加例子

--- vue3/eventbus.ts
+++ vue3/eventbus.ts
@@ -15,3 +15,22 @@ class EventBus {
     })
   }
 }
+  // 初始化EventBus
+  let EB = new EventBus();
+
+
+  // 订阅事件
+  EB.$on('key1', (name, age) => {
+    console.info("我是订阅事件A:", name, age);
+  })
+  EB.$on("key1", (name, age) => {
+    console.info("我是订阅事件B:", name, age);
+  })
+  EB.$on("key2", (name) => {
+    console.info("我是订阅事件C:", name);
+  })
+
+
+  // 发布事件
+  EB.$emit('key1', "小猪课堂", 26);
+  EB.$emit('key2', "小猪课堂");
